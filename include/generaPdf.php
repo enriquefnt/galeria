1db096be278fa516bef03a49c31978c32306a907
enriquefnt <enriquefinetti@gmail.com>
2022-08-24 15:10:16 -0300

    avance

--- include/generaPdf.php
+++ include/generaPdf.php
@@ -1,14 +1,15 @@
 <?php
-include __DIR__ . '/../include/conect.php';
-include __DIR__ . '/../include/funciones.php';
-//include_once('fpdf.php');
 
-
+define('FPDF_FONTPATH','../font/');
 require('fpdf.php');
 
-$pdf = new FPDF();
+include __DIR__ . '/../include/conect.php';
+include __DIR__ . '/../include/funciones.php';
+
+
+$pdf=new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'¡Hola, Mundo!');
+$pdf->Cell(40,10,'¡Mi primera página pdf con FPDF!');
 $pdf->Output();
 ?>
