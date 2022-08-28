b4b8d808f86796bac69c2aba55aaed54b4e29793
enriquefnt <enriquefinetti@gmail.com>
2022-08-28 13:41:26 -0300

    fpd_casifinal

--- include/pdf_informe.php
+++ include/pdf_informe.php
@@ -2,39 +2,68 @@
 
 define('FPDF_FONTPATH','../font/');
 require('fpdf.php');
+
+
 include __DIR__ . '/../include/conect.php';
 include __DIR__ . '/../include/funciones.php';
 include __DIR__ . '/../include/clases.php';
 
-$query = $pdo->prepare('call `saltaped_actividades-promo`.paraInfPdf(133);');
+$query = $pdo->prepare('call `saltaped_actividades-promo`.paraInfPdf(152);');
     	$query->execute();
     	$actiPdf = $query->fetch();	
 
+$sql='call fotosActividad(152);';
+$imagenesActividad = $pdo_ub->query($sql);
+//$totalImagenes = $imagenesActividad->rowCount();
+
 $title= utf8_decode('Informe de actividad de Promoción');
 $texto= utf8_decode($actiPdf['descri']);
+
 
 
 $pdf=new PDF();
 $pdf->Open();
 $pdf->AddPage();
 $pdf->SetFont('Arial','',12);
-$pdf->Cell(0,8,utf8_decode('Área Operativa: '.$actiPdf['areaoperativa']),0,0);
+$pdf->Cell(0,7,utf8_decode('Actividad: '.$actiPdf['titulo']),0,0);
 $pdf->Ln();
-$pdf->Cell(0,8,utf8_decode('Lugar: '.$actiPdf['locale']),0,0);
+$pdf->SetFont('Arial','',12);
+$pdf->Cell(0,7,utf8_decode('Área Operativa: '.$actiPdf['areaoperativa']),0,0);
 $pdf->Ln();
-$pdf->Cell(0,8,utf8_decode('Tema principal: '.$actiPdf['temas']),0,0);
+$pdf->Cell(0,7,utf8_decode('Fecha: '.$actiPdf['fecha']),0,0);
+$pdf->Ln();
+
+$pdf->Cell(0,7,utf8_decode('Lugar: '.$actiPdf['locale']),0,0);
+$pdf->Ln();
+$pdf->Cell(0,7,utf8_decode('Tema principal: '.$actiPdf['temas']),0,0);
 if (isset($actiPdf['modalidades'])) {
 	$pdf->Ln();
-$pdf->Cell(0,8,utf8_decode('Modalidad: '.$actiPdf['modalidades']),0,0);
+$pdf->Cell(0,7,utf8_decode('Modalidad: '.$actiPdf['modalidades']),0,0);
 }
 $pdf->Ln();
-$pdf->Multicell(0,5,utf8_decode('  '.$actiPdf['descri']),0,'L');
-$pdf->WriteHTML('You can<br><p align="center">center a line</p>and add a horizontal rule:<br><hr>');
-//$pdf->WriteHTML("<div style='text-align: justify;'".$texto."</div>");
+$pdf->Cell(0,7,utf8_decode('Dirigido a: '.$actiPdf['participantes']),0,'L');
+$pdf->WriteHTML('<div><p align="center">'.$texto.'<br>Participaron '.$actiPdf['total'].' personas. </p></div>');
+$pdf->Ln(15);
 
+$pdf->SetFont('Arial','B',12);
+//$pdf->Cell(25);
+$pdf->Cell(0,7,utf8_decode('Imágenes:'),0,0,'B');
+$current_x = $pdf->GetX();
+$current_y = $pdf->GetY();
+$count=0;
+$xy=10;
 
-$pdf->Image('../imagenes/cierreLM.jpg',10 ,98, 55, 38 );
-$pdf->Image('../imagenes/IMG-20220801-WA0038.jpg',70 ,98, 55, 38 );
-$pdf->Image('../imagenes/IMG-20220801-WA0047.jpg',130 ,98, 55, 38 );
+foreach ($imagenesActividad as $imagen): 
+if ($count > (0-1) && $count < 3) {
+	$pdf->Image($imagen['archivo'],$xy ,$current_y+9, 58, 38 );
+}
+$count =$count+1;
+$xy = $xy+60;
+endforeach;
+
+$pdf->SetY($current_y+58);
+$pdf->SetFont('Arial','',12);
+$pdf->Cell(0,7,utf8_decode('Referente: '.$actiPdf['referente']),0,0);
+$pdf->Ln();
 $pdf->Output();
 ?>
